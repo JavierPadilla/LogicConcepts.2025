8dad8827ad73424d0fd653ce57a66f8228814604
JavierPadilla <host_javier@hotmail.com>
2025-07-22 15:20:36 -0700

    Eje01ParONo

---
+++ LogicaProg/Eje01ParOno/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Eje01ParOno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un numero entero  : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n%2==0)
+            {
+                Console.WriteLine("El numero {0} es Par", n);
+
+            }
+            else
+            {
+                Console.WriteLine("El numero {0 es impar }",n);
+            }
+        }
+    }
+}
