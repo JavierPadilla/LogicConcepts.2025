0f050a93ad9fd479e260a554b9a8b6a05d9d5da9
JavierPadilla <host_javier@hotmail.com>
2025-07-23 00:15:44 -0700

    Fin Eje01

--- LogicaProg/Eje01ParOno/Program.cs
+++ LogicaProg/Eje01ParOno/Program.cs
@@ -4,17 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Introduce un numero entero  : ");
-            int n = Convert.ToInt32(Console.ReadLine());
-            if (n%2==0)
+            string numeroString=string.Empty;
+            do
             {
-                Console.WriteLine("El numero {0} es Par", n);
+                Console.Write("Introduce un numero entero  o presione 's' para Salir : ");
+                numeroString = Console.ReadLine();
+                if (numeroString.ToLower() = "s")
+                {
+                    continue;
+                }
+                
+                int numeroEntero = 0;
+                if (int.TryParse(numeroString,out numeroEntero))
+                {
+                    if (n % 2 == 0)
+                    {
+                        Console.WriteLine("El numero {0} es Par", numeroEntero);
 
-            }
-            else
-            {
-                Console.WriteLine("El numero {0 es impar }",n);
-            }
+                    }
+                    else
+                    {
+                        Console.WriteLine("El numero {0 es impar }", numeroEntero);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Lo que ingresaste {0} no es un numero entero ",numeroString);
+                }
+
+
+            } while(numeroString.ToLower()!="s")
+            
         }
     }
 }
