5bf2a907116f78f10475fa1c472eead75c380c84
JavierPadilla <host_javier@hotmail.com>
2025-09-01 10:54:33 -0700

    FinEje07

--- LogicaProg/Eje07Descuentos/Program.cs
+++ LogicaProg/Eje07Descuentos/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Eje07Descuentos
+﻿using System.Diagnostics;
+using shared;
+
+namespace Eje07Descuentos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Programa para un almacen \n" +
+    "Calcula el total a pagar en base al numero de escritorios que el cliente lleva en su compra\n" +
+    "Cada escritorio tiene un valor de $6500.0" +
+    "\n" +
+    "se otorga un 10% en la compra de menos de 5 unidades" +
+    "se otorga un 20% en la compra de 5 a 9 unidades" +
+    "se otorga un 40% en la compra de 10 o mas unidesdes\n");
+
+            var answer = string.Empty;
+            var options = new List<string> { "s", "n" };
+
+            do
+            {
+
+                int esc = Aga.PidaEntero("Numero de Escritorios: ");
+                double desc = 0.0;
+                if (esc < 5) desc = (esc * 6500) * 0.1;
+                if (esc < 10 && esc > 4) desc = (esc * 6500) * 0.2;
+                if (esc > 9) desc = (esc * 6500) * 0.4;
+                Console.WriteLine($"El valor a pagar es de : $ {(esc * 6500) - desc:C3}");
+
+                do
+                {
+                    answer = Aga.GetValidOptions("Deseas continuar [s]i   [n]o : ", options);
+                } while (!options.Any(x => x.Equals(answer, StringComparison.InvariantCultureIgnoreCase)));
+
+            } while (answer!.Equals("s", StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
--- LogicaProg/IsLeapYear/Program.cs
+++ LogicaProg/IsLeapYear/Program.cs
@@ -9,35 +9,49 @@ namespace isLeapYear
             Console.WriteLine("--------\t Ejercicio numero 5 \n" +
                 "--------\t Programa que pide el anio como un numero entero \n" +
                 "--------\t y deptermina se es o no un anio bisciesto \n");
-            int year = Aga.PidaEntero("Introduce el year : ");
-            var currentyear = DateTime.Now.Year;
-            var me = string.Empty;
 
-            if (year > currentyear)
+            var answer = string.Empty;
+            var options = new List<string> { "s", "n" };
+
+            do
             {
-                me = "SERA";
-            }
-            else
-            {
-                if (year < currentyear)
+                int year = Aga.PidaEntero("Introduce el year : ");
+                var currentyear = DateTime.Now.Year;
+                var me = string.Empty;
+
+                if (year > currentyear)
                 {
-                    me = "FUE";
+                    me = "SERA";
                 }
                 else
                 {
-                    me = "ES";
+                    if (year < currentyear)
+                    {
+                        me = "FUE";
+                    }
+                    else
+                    {
+                        me = "ES";
+                    }
                 }
-            }
 
-            if (year%400==0 || year%4==0 && year%100 !=0)
-            {
-                Console.WriteLine($"\nEL anio {year} {me} bisciesto");
+                if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+                {
+                    Console.WriteLine($"\nEL anio {year} {me} bisciesto");
 
-            }
-            else
-            {
-                Console.WriteLine($"\nEl anio no {year} {me} anio bisiesto");
-            }
+                }
+                else
+                {
+                    Console.WriteLine($"\nEl anio no {year} {me} anio bisiesto");
+                }
+
+
+                do
+                {
+                    answer = Aga.GetValidOptions("Deseas continuar [s]i   [n]o : ", options);
+                } while (!options.Any(x => x.Equals(answer, StringComparison.InvariantCultureIgnoreCase)));
+
+            } while (answer!.Equals("s", StringComparison.InvariantCultureIgnoreCase)); 
                 
         }
     }
