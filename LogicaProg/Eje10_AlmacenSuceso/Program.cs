1f4e057ad9b20a139de10bd6ac1ce30c9b07c788
JavierPadilla <host_javier@hotmail.com>
2025-11-19 03:00:45 -0600

    IniEje10

---
+++ LogicaProg/Eje10_AlmacenSuceso/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Eje10_AlmacenSuceso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t Ejercicio 10: Almacenes Suceso\n" +
+                "Almacenes Suceso S.A Desea saber el Valor de Venta de un producto VR_V, a\n " +
+                "partir de la siguiente información que posee de este.\n\n" +
+                "Costo de compra CC.\n" +
+                "Tipo de producto TP (Perecedero o no perecedero).\n" +
+                "Tipo de conservacion TC (Frio o ambiente).\n" +
+                "Periodo de conservación en diás PC.\n" +
+                "");
+        }
+    }
+}
